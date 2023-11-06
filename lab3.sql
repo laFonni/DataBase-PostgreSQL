bff671b4a6e3f32bdbd9256fc4dccfed3c004a6b
Olaf Grykalowski <olaf.grykalowski@gmail.com>
2023-11-06 22:43:35 +0100

    feat: 3.6

--- lab3.sql
+++ lab3.sql
@@ -363,3 +363,21 @@ WHERE nazwa LIKE '%rz%' OR nazwa LIKE '%Rz%'
 (SELECT idczekoladki FROM czekoladki)
 EXCEPT 
 (SELECT idczekoladki FROM zawartosc)
+
+/* Exercise 6 */
+
+-- 1. identyfikator meczu, sumę punktów zdobytych przez gospodarzy i sumę punktów zdobytych przez gości,
+
+SELECT 
+	idmeczu,
+	(SELECT SUM(s) FROM UNNEST(gospodarze) s) as gospodarze,
+	(SELECT SUM(s) FROM UNNEST(goscie) s) as goscie
+FROM siatkowka.statystyki
+
+-- 2. identyfikator meczu, sumę punktów zdobytych przez gospodarzy i sumę punktów zdobytych przez gości, dla meczów, które skończyły się po 5 setach i zwycięzca ostatniego seta zdobył w nim ponad 15 punktów,
+
+
+
+-- 3. identyfikator i wynik meczu w formacie x:y, np. 3:1 (wynik jest pojedynczą kolumną – napisem),
+-- 4. ★ identyfikator meczu, sumę punktów zdobytych przez gospodarzy i sumę punktów zdobytych przez gości, dla meczów, w których gospodarze zdobyli ponad 100 punktów,
+-- 5. ★ identyfikator meczu, liczbę punktów zdobytych przez gospodarzy w pierwszym secie, sumę punktów zdobytych w meczu przez gospodarzy, dla meczów, w których pierwiastek kwadratowy z liczby punktów zdobytych przez gospodarzy w pierwszym secie jest mniejszy niż logarytm o podstawie 2 z sumy punktów zdobytych w meczu przez gospodarzy. ;)
