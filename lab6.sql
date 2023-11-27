ba9c1d05a439803a5814bf90229841fceb01099c
Olaf Grykalowski <olaf.grykalowski@gmail.com>
2023-11-27 14:45:30 +0100

    feat: 6.3 and 6.4

--- lab6.sql
+++ lab6.sql
@@ -16,21 +16,62 @@ VALUES
 	('W98', 'Biały kieł', 'biała', 'laskowe', 'marcepan','Rozpływające się w rękach i kieszeniach', 0.45, 20)
 
 
--- Napisz i wykonaj zapytanie, które doda do tabeli czekoladki następujące pozycje, wykorzystaj wartości NULL w poleceniu INSERT:
--- IDCzekoladki: X91,
--- Nazwa: Nieznana Nieznajoma,
--- Opis: Niewidzialna czekoladka wspomagajaca odchudzanie.,
--- Koszt: 26 gr,
--- Masa: 0g,
+INSERT INTO
+	klienci (idklienta, nazwa, ulica, miejscowosc, kod, telefon)
+VALUES
+	(90, 'Matusiak Edward', 'Kropiwnickiego 6/3', 'Leningrad', '31-471', '031 423 45 38'),
+	(91, 'Matusiak Alina', 'Kropiwnickiego 6/3', 'Leningrad', '31-471', '031 423 45 38'),
+	(92, 'Kimono Franek', 'Karateków 8', 'Mistrz', '30-029', '501 498 324')
 
--- IDCzekoladki: M98,
--- Nazwa: Mleczny Raj,
--- Czekolada: Mleczna,
--- Opis: Aksamitna mleczna czekolada w ksztalcie butelki z mlekiem.,
--- Koszt: 26 gr,
--- Masa: 36 g,
 
-INSERT INTO 
-	czekoladki ()
+INSERT INTO
+	klienci (idklienta, nazwa, ulica, miejscowosc, kod, telefon)
+VALUES
+(
+	93,
+	(SELECT nazwa FROM klienci WHERE idklienta = 90),
+	(SELECT ulica FROM klienci WHERE idklienta = 90),
+	(SELECT miejscowosc FROM klienci WHERE idklienta = 90),
+	(SELECT kod FROM klienci WHERE idklienta = 90),
+	(SELECT telefon FROM klienci WHERE idklienta = 90)
+)
+
+
 
 /*Exercise 2*/
+
+INSERT INTO 
+	czekoladki (idczekoladki,nazwa, czekolada, opis, koszt, masa)
+VALUES
+	('X91', 'Niewidzialna Nieznajoma', NULL , 'Niewidzialna czekoladka wspomagajaca odchudzanie.', 0.26, 0),
+	('M98', 'Mleczny Raj', 'mleczna',  'Aksamitna mleczna czekolada w ksztalcie butelki z mlekiem.', 0.26, 36)
+
+/*Exercise 3*/
+
+DELETE FROM czekoladki WHERE idczekoladki IN ('X91', 'M98')
+
+/*Exercise 4*/
+
+-- 1. Zmiana nazwiska Izy Matusiak na Nowak.
+-- Obniżenie kosztu czekoladek o identyfikatorach (idczekoladki): W98, M98 i X91, o 10%.
+
+UPDATE klienci SET nazwa = 'Iza Nowak' WHERE idklienta = 93
+
+
+-- 2. Ustalenie kosztu czekoladek o nazwie Nieznana Nieznajoma na taką samą jak cena czekoladki o identyfikatorze W98.
+
+UPDATE czekoladki SET koszt = (SELECT koszt FROM czekoladki WHERE idczekoladki = 'W98') WHERE nazwa = 'Niewidzialna Nieznajoma'
+
+--OR
+
+UPDATE czekoladki cz1 SET koszt = cz2.koszt
+FROM czekoladki cz2
+WHERE cz1.nazwa = 'Niewidzialna Nieznajoma' AND cz2.idczekoladki = 'W98';
+
+-- 3. ★ Zmiana nazwy z Leningrad na Piotrograd w tabeli klienci.
+
+UPDATE klienci SET miejscowosc='Piotrgard' WHERE miejscowosc = 'Leningrad'
+
+-- 4. ★ Podniesienie kosztu czekoladek, których dwa ostatnie znaki identyfikatora (idczekoladki) są większe od 90, o 15 groszy.
+
+UPDATE czekoladki cz SET koszt = cz.koszt + 0.15 WHERE substr(cz.idczekoladki, 2, 2)::int > 90
